0b6292ce88a761d5217f05cbf7a6df1c50847f88
LobsterCheese <68477510+LobsterCheese@users.noreply.github.com>
2025-11-15 11:18:01 -0500

    refactored textbox code
    cleaned up a lot of useless code, code is now a lot cleaner and less clunky, functionality remains the same

--- Assets/Anderson Stuff/TextBox.cs
+++ Assets/Anderson Stuff/TextBox.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System.Collections;
 
 public class TextBox : MonoBehaviour
 {
@@ -12,20 +13,14 @@ public class TextBox : MonoBehaviour
 
     //[SerializeField] sprites (figure out how to implement that)
 
-    //use these to keep how long a line of text is and which page of the text you're currently on
-    [SerializeField] private int textLineLength;
-    [SerializeField] private int currentPage = 0;
-    [SerializeField] private int maxPages;
+    //keeps track of page
+    [SerializeField] private int index;
 
-    //current letter the text is on, for typewriter style
-    [SerializeField] private int currentLetter = 0;
-    [SerializeField] private int maxLetter;
-    [SerializeField] private int typeSpeed = 1;
+    //how fast the text types
+    [SerializeField] private float typeSpeed = 0.005f;
 
-    //[SerializeField] Array arrText;
+    //actual dialogue
     [SerializeField] private string[] testText = new string[] { "yo!", "i'm testing this out dude", "lets see how it goes", "this is the last line" };
-
-    //[SerializeField] string text;
 
     private GameObject self;
 
@@ -34,70 +29,42 @@ public class TextBox : MonoBehaviour
     private void Awake()
     {
         self = this.gameObject;
-        //textBoxDisplay = GetComponent();
-        //textBoxDisplay.GetComponent<TMPro.TextMeshProUGUI>().text = "if this is showing up you fucked up";
     }
 
     void Start()
     {
-        //textBoxDisplay = GetComponent<TMP_Text>();
-        currentPage = 0;
-        currentLetter = 0;
 
-        textBoxDisplay.text = testText[currentPage].ToString();
-
-        //textBoxDisplay.text = "if this is showing up you fucked up";
-
-        //textBoxDisplay.GetComponent<TMPro.TextMeshProUGUI>().text = "if this is showing up you fucked up";
-
-        maxPages = testText.Length;
-        //Debug.Log(maxPages);
     }
 
-    // Update is called once per frame
+    void nextSentence()
+    {
+        if(index < testText.Length)
+        {
+            textBoxDisplay.text = "";
+            StartCoroutine(WriteSentence());
+        }
+        else
+        {
+            index = 0;
+            textBoxDisplay.text = "";
+            self.SetActive(false);
+        }
+    }
+
+    IEnumerator WriteSentence()
+    {
+        foreach (char Character in testText[index].ToCharArray()){
+            textBoxDisplay.text += Character;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+        index++;
+    }
+
     void Update()
     {
-
-        Debug.Log("Current page is: " + currentPage);
-        Debug.Log("the max pages are: " + maxPages);
-
-        if (currentPage < maxPages)
-        {
-            maxLetter = testText[currentPage].ToString().Length;
-            textBoxDisplay.text = testText[currentPage].ToString().Substring(0, currentLetter);
-
-            if(currentLetter < maxLetter)
-            {
-                currentLetter++;
-            }
-            else
-            {
-                currentLetter = maxLetter;
-            }
-        }
-
         if (Input.GetKeyDown("space"))
         {
-
-            currentPage++;
-
-            //typewriter style
-            if (currentLetter < maxLetter)
-            {
-                currentLetter = maxLetter;
-            }
-            else
-            {
-                currentLetter = 0;
-            }
-
-            //kills the box and resets everything
-            if (currentPage >= maxPages)
-            {
-                currentPage = 0;
-                currentLetter = 0;
-                self.SetActive(false);
-            }
+            nextSentence();
         }
     }
 }
