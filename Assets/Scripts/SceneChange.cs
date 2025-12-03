2bb99418463f23adb42c67b7fa98b4a286094795
GoldP706 <jd060207@gmail.com>
2025-12-03 12:58:27 -0500

    Sprites n animations

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -5,7 +5,7 @@ public class SceneChange : MonoBehaviour
 {
     [SerializeField]string thisScene;
 
-    void OnTriggerEnter2D(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         SceneManager.LoadScene(thisScene);
     }
