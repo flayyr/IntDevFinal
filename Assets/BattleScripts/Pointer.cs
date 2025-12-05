623af6b8f3945230722ea1cbe735b8d6023ecb26
flayyr <43307106+flayyr@users.noreply.github.com>
2025-12-05 18:19:42 -0500

    Combat UI Polish
    Added animated pointer name tag.

--- Assets/BattleScripts/Pointer.cs
+++ Assets/BattleScripts/Pointer.cs
@@ -13,9 +13,9 @@ public class Pointer : MonoBehaviour
     {
         transform.position = entity.transform.position;
         nameTag.transform.position = transform.position + textOffSet;
-        nameText.text = entity.entityName;
+        //nameText.text = entity.entityName;
         gameObject.SetActive(true);
-        nameText.gameObject.SetActive(true);
+        //nameText.gameObject.SetActive(true);
         if(entity.wideAngled && hpBar != null) {
             hpBar.gameObject.SetActive(true);
             hpBar.value = (float)entity.hp / entity.maxHP;
@@ -37,7 +37,7 @@ public class Pointer : MonoBehaviour
     public void Hide()
     {
         transform.position = new Vector3(-100, -100, 0);
-        nameText.transform.position = transform.position + textOffSet;
+        //nameText.transform.position = transform.position + textOffSet;
     }
 
 }
