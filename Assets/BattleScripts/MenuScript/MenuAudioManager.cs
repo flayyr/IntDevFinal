6f3833699d64c982b21ad4bb40ce02ef96478e4a
GoldP706 <jd060207@gmail.com>
2025-12-08 11:42:49 -0500

    Fixed Menu Portraits

---
+++ Assets/BattleScripts/MenuScript/MenuAudioManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MenuAudioManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z)){
+            
+        }
+    }
+
+}
